51540d33ce06f878a843b0074a809cb3e12eebf1
adhil123-git <aadhi0191@gmail.com>
2025-05-04 22:47:20 +0530

    updated

--- backend/src/main/java/com/example/backend/model/User.java
+++ backend/src/main/java/com/example/backend/model/User.java
@@ -15,13 +15,6 @@ public class User {
 
     private String password;
 
-    public User() {}
-
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
    
     public Long getId() {
         return id;
